9ca119ca8c7b29f6b6a875c327673c9c73352d8f
Viktor Varenik <yavarenikya@gmail.com>
2025-10-11 22:00:12 +0300

    feat: full features tooltips about pickups

--- src/client/kotlin/kotleni/pickupnotif/client/PickupnotifClient.kt
+++ src/client/kotlin/kotleni/pickupnotif/client/PickupnotifClient.kt
@@ -2,14 +2,74 @@ package kotleni.pickupnotif.client
 
 import kotleni.pickupnotif.ItemPickupCallback
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder
+import net.minecraft.client.gui.widget.ClickableWidget
+import net.minecraft.client.gui.widget.Widget
+import net.minecraft.client.render.Tessellator
+import net.minecraft.entity.boss.BossBar
 import net.minecraft.item.ItemStack
 import net.minecraft.text.Text
+import net.minecraft.util.Colors
+import java.util.function.Consumer
+//
+//class TestWidget : Widget {
+//    override fun setX(x: Int) {
+//
+//    }
+//
+//    override fun setY(y: Int) {
+//
+//    }
+//
+//    override fun getX(): Int {
+//
+//    }
+//
+//    override fun getY(): Int {
+//
+//    }
+//
+//    override fun getWidth(): Int {
+//
+//    }
+//
+//    override fun getHeight(): Int {
+//
+//    }
+//
+//    override fun forEachChild(consumer: Consumer<ClickableWidget?>?) {
+//
+//    }
+//}
+
+data class PickupMessage(
+    val itemName: String,
+    var itemCount: Int,
+    var itemsTotal: Int,
+    var createTime: Long
+)
 
 class PickupnotifClient : ClientModInitializer {
     private val client: MinecraftClient get() = MinecraftClient.getInstance();
+    private var pickupMessages: ArrayList<PickupMessage> = arrayListOf();
 
     override fun onInitializeClient() {
+        HudRenderCallback.EVENT.register { drawContext, tickCounter ->
+            pickupMessages.forEachIndexed { index, message ->
+                if(System.currentTimeMillis() - message.createTime > 2000) return@forEachIndexed
+                drawContext.drawText(
+                    client.inGameHud.textRenderer,
+                    "${message.itemName} +${message.itemCount} (${message.itemsTotal})",
+                    40,
+                    40 + (10 * index),
+                    Colors.ALTERNATE_WHITE,
+                    false
+                );
+            }
+        }
         ItemPickupCallback.EVENT?.register { stack ->
             client.execute {
                 onPickupItem(stack)
@@ -18,7 +78,25 @@ class PickupnotifClient : ClientModInitializer {
     }
 
     private fun onPickupItem(stack: ItemStack) {
-        val message = "Picked up: ${stack.count}x ${stack.name.string}"
-        client.inGameHud.chatHud.addMessage(Text.of(message))
+        pickupMessages.removeIf { System.currentTimeMillis() - it.createTime >= 2000 }
+
+        val totalCount = client.player?.inventory
+            ?.toList()
+            ?.filter { it.itemName == stack.itemName }
+            ?.map { it.count }
+            ?.reduce { a, b -> a + b } ?: -1
+
+        val prevMessage = pickupMessages.find { it.itemName == stack.itemName.string }
+        if(prevMessage != null) {
+            prevMessage.itemCount += stack.count;
+            prevMessage.createTime = System.currentTimeMillis();
+        } else {
+            pickupMessages.add(PickupMessage(
+                stack.name.string,
+                stack.count,
+                totalCount,
+                System.currentTimeMillis()
+            ))
+        }
     }
 }
